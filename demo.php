d1ef2cb81962fbf36bc2a636d9726aec169d6acf
cevin <cevin.cheung@gmail.com>
2014-10-11 12:27:40 +0800

    add demo

---
+++ demo.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once 'geohash.class.php';
+
+$length = 6;
+
+$geohash = new Geohash();
+
+$hash = $geohash->encode('latitude','longtitude');
+
+$box = $geohash->neighbors(substr($hash,0,$length));
+
+$box[] = substr($hash,0,$length);
+
+
+$in_str = "'".implode("','", $box)."'";
+
+// MYSQL
+// SELECT FROM `table` WHERE LEFT(geohash, $length) IN ($in_str)
+// SELECT FROM `table` WHERE LEFT(geohash, 6) IN ('xxxxxx','aaaaaa', ......)
